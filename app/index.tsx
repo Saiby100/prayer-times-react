52ef05d0a2fc38d46be293f30999307741d48794
Salahuddin Saiet <salahuddin@lowdefy.com>
2025-02-09 21:18:49 +0200

    Remove unused import

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import React, {  useEffect, useState } from "react";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import { useRouter, useFocusEffect } from 'expo-router';
 import PTApi from "../utils/PTApi";
